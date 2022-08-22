6e5d4e013d8a5113ab788251a3034172d745b68c
minongarm2arm <108971708+minongarm2arm@users.noreply.github.com>
2022-08-22 22:24:45 +0900

    Revert "메인 페이지 벽돌식 레이아웃"

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,10 @@
 // 메인 페이지
-// import InfiniteScroll from "../components/Main/InfiniteScroll";
-// import FinalBoard from '../components/Main/FinalBoard';
 
 // CSS 불러오기
 import styled from "styled-components";
+import MasonryInfiniteScroll from '../components/Main/InfiniteScroll';
 
-function Login(){
+function Main() {
 
   return(
       <Content>
@@ -13,13 +12,19 @@ function Login(){
           메인 페이지
         </strong>
 
-        {/* <FinalBoard/> */}
-        {/* <InfiniteScroll/> */}
+        <MasonryInfiniteScroll>
+          <div><img className="item" alt="img01" src="images/img01.png" /></div>
+          <div><img className="item" alt="img02" src="images/img02.png" /></div>
+          <div><img className="item" alt="img03" src="images/img03.png" /></div>
+          <div><img className="item" alt="img04" src="images/img04.png" /></div>
+          <div><img className="item" alt="img05" src="images/img05.png" /></div>
+          <div><img className="item" alt="img06" src="images/img06.png" /></div>
+        </MasonryInfiniteScroll>
       </Content>
   )
 }
 
-export default Login;
+export default Main;
 
 const Content = styled.div`
   text-align: center;
